8b3a88a8a427977e210ae6cba8e955fa6790b700
mohabroha@mail.de <mohabroha@mail.de>
2026-06-26 16:51:57 +0200

    fix: create user profile on registration

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
 
+from profile_app.models import Profile
+
 User = get_user_model()
 
 
@@ -10,6 +12,8 @@ class RegisterSerializer(serializers.ModelSerializer):
     """
 
     password = serializers.CharField(write_only=True)
+    repeated_password = serializers.CharField(write_only=True)
+    type = serializers.ChoiceField(choices=Profile.USER_TYPE)
 
     class Meta:
         """
@@ -17,17 +21,42 @@ class RegisterSerializer(serializers.ModelSerializer):
         """
 
         model = User
-        fields = ["id", "username", "password", "email"]
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "repeated_password",
+            "type",
+        ]
+
+    def validate(self, attrs):
+        """
+        Validate the registration data.
+        """
+        if attrs["password"] != attrs["repeated_password"]:
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+
+        return attrs
 
     def create(self, validated_data):
         """
-        Create and return a new user with the validated registration data.
+        Create a new user and the corresponding profile.
         """
+        validated_data.pop("repeated_password")
+        user_type = validated_data.pop("type")
+
         user = User.objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email"),
             password=validated_data["password"],
         )
+
+        Profile.objects.create(
+            user=user,
+            type=user_type,
+        )
+
         return user
 
 
@@ -44,7 +73,10 @@ class LoginSerializer(serializers.Serializer):
         Authenticate the user and attach the authenticated user instance
         to the validated data.
         """
-        user = authenticate(username=data["username"], password=data["password"])
+        user = authenticate(
+            username=data["username"],
+            password=data["password"],
+        )
 
         if not user:
             raise serializers.ValidationError("Invalid credentials")
